77980735591ff3716eba21f56cef80ebd1d3361c
madonahs <syombuamadona@gmail.com>
2020-07-19 10:21:01 -0500

    clean up

--- app/src/main/java/com/madonasyombua/sportsdb/MainActivity.kt
+++ app/src/main/java/com/madonasyombua/sportsdb/MainActivity.kt
@@ -3,8 +3,6 @@ package com.madonasyombua.sportsdb
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-import com.madonasyombua.sportsdb.R
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
